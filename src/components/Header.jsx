afcfca74504a657758119613fba2cf09d561f996
Mohd kaif <shaikhkaif3120@gmail.com>
2021-06-21 21:31:04 +0530

    added customised styling and material-ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 
-function Header(){
-
-    return (<header>
-    <h1>Keeper</h1>
-    </header>)
+function Header() {
+  return (
+    <header>
+    
+    <h1>
+    <LibraryBooksIcon>  </LibraryBooksIcon>
+    &nbsp; Sticky Notes</h1>
+    </header>
+  );
 }
 
-export default Header; 
+export default Header;
